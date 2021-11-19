53c5a27580c18d7b2a0b6e420e4c62bde0cff1ca
Davi <sbernardo.davi@outlook.com>
2021-11-19 18:10:47 -0300

    fix: corrigindo banco de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,48 +2,48 @@ insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(0, 'Product Number 0', '//imagemUrlns.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538', 363, 332, 'Product long description number 0 just to make more than one like of text.', 0);
 
 insert into parcela(id, numParcelas, value)
-values(0, 10, 33.2);
+values(0, 10, 33.20);
 
 insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(1,'Product Number 1','//imagemUrlns.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6829307', 104, 76, 'Product long description number 1 just to make more than one like of text.', 1);
 
 insert into parcela(id, numParcelas, value)
-values(1, 10, 7.6);
+values(1, 10, 7.60);
 
 insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(2, 'Product Number 2','//imagemUrlns.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6506376', 154, 134, 'Product long description number 2 just to make more than one like of text.', 2);
 
 insert into parcela(id, numParcelas, value)
-values(2, 10, 8.0);
+values(2, 10, 8.00);
 
 insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(3, 'Product Number 3','//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6584703', 292, 272, 'Product long description number 3 just to make more than one like of text.', 3);
 
 insert into parcela(id, numParcelas, value)
-values(3, 10, 27.2);
+values(3, 10, 27.20);
 
 insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(4, 'Product Number 4','//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6875461', 139, 92, 'Product long description number 4 just to make more than one like of text.', 4);
 
 insert into parcela(id, numParcelas, value)
-values(4, 10, 9.2);
+values(4, 10, 9.20);
 
 insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(5, 'Product Number 5','//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6875461', 112, 68, 'Product long description number 4 just to make more than one like of text.', 5);
 
 insert into parcela(id, numParcelas, value)
-values(5, 10, 6.8);
+values(5, 10, 6.80);
 
 insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(6, 'Product Number 6','//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6875461', 140, 114, 'Product long description number 4 just to make more than one like of text.', 6);
 
 insert into parcela(id, numParcelas, value)
-values(6, 10, 11.4);
+values(6, 10, 11.40);
 
 insert into produto(id, nome, imagemUrl, oldPrice, price, descricao, parcela_id)
 values(7, 'Product Number 7','//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6875461', 213, 201, 'Product long description number 4 just to make more than one like of text.', 7);
 
 insert into parcela(id, numParcelas, value)
-values(7, 10, 20.1);
+values(7, 10, 20.10);
 
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
 create table produto (
     id int not null,
-    nome VARCHAR(90) not null,
-    imagemUrl VARCHAR(255) not null,
+    nome VARCHAR(100) not null,
+    imagemUrl VARCHAR(300) not null,
     oldPrice float not null,
     price float not null,
-    descricao varchar(90),
+    descricao varchar(200),
     parcela_id int not null
 );
 
